b82c09fbf1225b2b4fa2df1cc4dbc6c029b8cc42
Caleb Stein <caleb@calebstein.net>
2025-10-11 14:39:32 -0700

    add test

--- sigprintf_test.c
+++ sigprintf_test.c
@@ -9,6 +9,7 @@ main(void)
 	(void)sigdputs("Hello, error world\n", STDERR_FILENO);
 	(void)sigprintf("Hello %%%d %s The end!\n", -133, "world!");
 	(void)sigdprintf(2, "Hello %%%d %s The err!\n", 1334, "world!");
+	(void)sigprintf("This should be zero: %d\n", 0);
 
 	return 0;
 }
